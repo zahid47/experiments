e7fb4127f279391d3ab313f971981029b7dac246
zahid <epiczahid@gmail.com>
2023-05-12 17:38:18 +0600

    mantine

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,20 +1,30 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import ReactSlider from "react-slider";
+import { Slider } from "@mantine/core";
 
-export default function Slider({ currentValue }: { currentValue: number }) {
+export default function SliderCustom({
+  currentValue,
+}: {
+  currentValue: number;
+}) {
   const router = useRouter();
 
   return (
-    <section>
-      <ReactSlider
-        className="horizontal-slider p-6"
+    <section className="flex justify-center p-10">
+      <Slider
+        w={300}
         defaultValue={currentValue}
         min={1}
         max={5}
-        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-        onAfterChange={(value) => {
+        marks={[
+          { value: 1, label: "1" },
+          { value: 2, label: "2" },
+          { value: 3, label: "3" },
+          { value: 4, label: "4" },
+          { value: 5, label: "5" },
+        ]}
+        onChangeEnd={(value) => {
           router.push(`/?limit=${value}`);
         }}
       />
