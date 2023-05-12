98b9e8b943547eae26d3b2a82958f5daf4c7c59d
zahid <epiczahid@gmail.com>
2023-05-12 17:23:48 +0600

    minor

--- src/actions/getTodos.ts
+++ src/actions/getTodos.ts
@@ -1,11 +1,9 @@
 import prisma from "../../prisma/prisma";
 
 async function getTodos(limit: number) {
-  const todos = await prisma.todos.findMany({
+  return await prisma.todo.findMany({
     take: limit,
   });
-
-  return todos;
 }
 
 export default getTodos;
