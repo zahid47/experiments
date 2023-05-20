0c27e973eec4f827b9efb987ca61bc7f323fccf3
zahid <epiczahid@gmail.com>
2023-05-21 03:22:44 +0600

    minor

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
+// export async function middleware(request: NextRequest) {
+//   return NextResponse.next();
+// }
 
 export { default } from "next-auth/middleware";
 export const config = { matcher: ["/secret"] };
