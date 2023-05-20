21d2db5e77ee36944f1c8d18eb520003210e2070
zahid <epiczahid@gmail.com>
2023-05-21 03:04:23 +0600

    minor

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Todo({ todo }: any) {
   return (
     <section>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Todo from "./Todo";
 
 export default function Todos({ todos }: any) {
