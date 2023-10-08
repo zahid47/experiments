11fe0371e45cb457e06aaaf6a02311e8d7f726a3
zahid47 <epiczahid@gmail.com>
2023-10-08 06:35:44 +0600

    realtime new fixes

--- app/supa-realtime/News.tsx
+++ app/supa-realtime/News.tsx
@@ -3,6 +3,14 @@
 import { useEffect, useState } from "react";
 import type { News } from "@prisma/client";
 import supa from "@/lib/supaClient";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface Props {
   initialNews: News[];
@@ -10,6 +18,7 @@ interface Props {
 
 export default function News({ initialNews }: Props) {
   const [news, setNews] = useState(initialNews);
+  const [orderAlert, setOrderAlert] = useState<News[] | null>(null);
 
   useEffect(() => {
     const channel = supa
@@ -20,6 +29,7 @@ export default function News({ initialNews }: Props) {
         (payload) => {
           const newNews = payload.new as News;
           setNews((prev) => [...prev, newNews]);
+          setOrderAlert((prev) => [...(prev ?? []), newNews]);
         }
       )
       .subscribe();
@@ -30,15 +40,38 @@ export default function News({ initialNews }: Props) {
   }, []);
 
   return (
-    <ul className="m-auto flex h-screen w-[50%] flex-col items-start justify-center gap-4">
-      {news.map((item) => (
-        <div>
-          <li key={item.id}>
-            <h2 className="text-lg">{item.headline}</h2>
-            <p className="text-gray-500">{item.content}</p>
-          </li>
-        </div>
-      ))}
-    </ul>
+    <>
+      <Dialog
+        open={Boolean(orderAlert)}
+        onOpenChange={() => {
+          setOrderAlert(null);
+        }}
+      >
+        <DialogContent className="h-screen min-w-[100%] bg-red-900">
+          <DialogHeader>
+            <DialogTitle>New Order(s) Received</DialogTitle>
+            <DialogDescription>
+              {orderAlert?.map((item) => (
+                <div key={item.id}>
+                  <h2 className="text-lg">{item.headline}</h2>
+                  <p className="text-gray-500">{item.content}</p>
+                </div>
+              ))}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      <ul className="m-auto flex h-screen w-[50%] flex-col items-start justify-center gap-4">
+        {news.map((item) => (
+          <div key={item.id}>
+            <li key={item.id}>
+              <h2 className="text-lg">{item.headline}</h2>
+              <p className="text-gray-500">{item.content}</p>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </>
   );
 }
--- lib/supaClient.ts
+++ lib/supaClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://sexmkezpkoamwtggwqar.supabase.co";
-const supabaseKey = process.env.SUPA_ANON_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPA_ANON_KEY!;
 
 const client = createClient(supabaseUrl, supabaseKey);
 
